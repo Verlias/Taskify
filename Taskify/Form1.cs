a765ff148da66eff99b6c568e99e1a0465f12748
Logan <Loganvoravong@gmail.com>
2024-07-04 22:14:03 -0400

    Implemented Task Class

--- Taskify/Form1.cs
+++ Taskify/Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Windows.Forms;
+using TaskClass;
+
+
 namespace Taskify
 {
     public partial class Form1 : Form
@@ -5,6 +10,14 @@ namespace Taskify
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            TaskObj Test = new TaskObj();
+            MessageBox.Show(Test.DisplayTaskDetails()); // Testing Display Task Detail Method Works
+            MessageBox.Show(TaskObj.Add()); //Testing Static Method
         }
     }
 }
---
+++ Taskify/TaskClass.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows.Forms;
+
+
+
+namespace TaskClass
+{
+    public class TaskObj
+    {
+
+        //Attributes
+        private string TaskTitle = "";
+        private string TaskDescription = "";
+        private string TaskDueDate = "";
+        private TaskPriority _TaskPriority = TaskPriority.Low;       //Low, Medium, High
+        private TaskStatus _TaskStatus = TaskStatus.NotStarted;         //Not Started, In Progress, Completed
+
+
+
+        public enum TaskPriority
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        public enum TaskStatus
+        {
+            NotStarted,
+            InProgress,
+            Completed
+        }
+        
+
+        //Getters and Setters: Properties
+        public string Tasktitle
+        {
+            get { return TaskTitle; }
+            set { TaskTitle = value; }
+        }
+
+        public string Taskdescription
+        {
+            get { return TaskDescription; }
+            set { TaskDescription = value; }
+        }
+
+        public string Taskduedate
+        {
+            get { return TaskDueDate; }
+            set { TaskDueDate = value; }
+        }
+
+        public TaskPriority Taskpriority
+        {
+            get { return _TaskPriority; }
+            set { _TaskPriority = value; }
+        }
+
+        public TaskStatus Taskstatus
+        {
+            get { return _TaskStatus; }
+            set { _TaskStatus = value; }
+        }
+
+        //Constructor: Allows for the Task to be created without any arguments passed
+        public TaskObj()
+        {
+
+        }
+
+
+        //Constructor: ALlows for the Task to be created with all arguments passed
+        public TaskObj(string tasktitle, string taskdescription, string taskduedate, TaskPriority _taskpriority, TaskStatus _taskstatus)
+        {
+            TaskTitle = tasktitle;
+            TaskDescription = taskdescription;
+            TaskDueDate = taskduedate;
+            _TaskPriority = _taskpriority;
+            _TaskStatus = _taskstatus;
+        }
+
+
+        //Method: Display Task Details
+        public string DisplayTaskDetails()
+        {
+            return "Task Title: " + TaskTitle + "\nTask Description: " + TaskDescription + "\nTask Due Date: " + TaskDueDate + "\nTask Priority: " + _TaskPriority + "\nTask Status: " + _TaskStatus;
+        }
+
+        public static string Add()
+        {
+            return "Task Title";
+        }
+    }
+}
