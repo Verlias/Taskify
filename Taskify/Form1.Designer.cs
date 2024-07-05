7be30777fea0c7c0e89dd8f86f60c6a036580298
Logan <Loganvoravong@gmail.com>
2024-07-05 16:16:37 -0400

    Implemented Add and Display Task Buttons

--- Taskify/Form1.Designer.cs
+++ Taskify/Form1.Designer.cs
@@ -29,31 +29,111 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(349, 197);
+            button1.Location = new Point(48, 171);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Add Tasks";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(659, 306);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Display Tasks";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(586, 26);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(202, 274);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(38, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(38, 55);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(38, 84);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Low", "Medium ", "High" });
+            comboBox1.Location = new Point(38, 113);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(100, 23);
+            comboBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { " NotStarted", " InProgress", " Completed" });
+            comboBox2.Location = new Point(38, 142);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(100, 23);
+            comboBox2.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Taskify";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
     }
 }
--- Taskify/Form1.cs
+++ Taskify/Form1.cs
@@ -6,21 +6,59 @@ namespace Taskify
 {
     public partial class Form1 : Form
     {
+
+        private TaskList tasklist;
+
+
         public Form1()
         {
+
+            tasklist = new TaskList(); //Instantiate TaskList
             InitializeComponent();
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Capture Inputs from TextBoxes and ComboBoxes
+            string TaskTitle = textBox1.Text;
+            string TaskDescription = textBox2.Text;
+            string TaskDueDate = textBox3.Text;
+       
+            TaskClass.TaskObj.TaskPriority TaskPriority = (TaskClass.TaskObj.TaskPriority)comboBox1.SelectedIndex;
 
-            TaskObj Test = new TaskObj();
-            TaskObj Test2 = new TaskObj("Hello World", "Hi", "tmr", TaskObj.TaskPriority.Low, TaskObj.TaskStatus.NotStarted);
-            TaskList list = new TaskList();
-            list.AddTask(Test);
-            list.AddTask(Test2);
-            list.DisplayTaskList();
-            list.RemoveTask(Test2);
+            if (comboBox1.SelectedIndex == -1)
+            {
+                TaskPriority = TaskClass.TaskObj.TaskPriority.Low;
+            }
+
+            TaskClass.TaskObj.TaskStatus TaskStatus = (TaskClass.TaskObj.TaskStatus)comboBox2.SelectedIndex;
+
+            if (comboBox2.SelectedIndex == -1)
+            {
+                TaskStatus = TaskClass.TaskObj.TaskStatus.NotStarted;
+            }
+
+
+            //Instantiate TaskObj
+            TaskObj Task = new TaskObj();
+            Task.Tasktitle = TaskTitle;
+            Task.Taskdescription = TaskDescription;
+            Task.Taskduedate = TaskDueDate;
+            Task.Taskpriority = TaskPriority;
+            Task.Taskstatus = TaskStatus;
+
+            //Add Task to TaskList
+            tasklist.AddTask(Task);
+
+            MessageBox.Show("Task Added");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tasklist.DisplayTaskList();
         }
     }
 }
